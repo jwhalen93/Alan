cc5a8d2429921ce7496fb29b9eb77dc9990e650b
Michael Gattozzi <mgattozzi@gmail.com>
2014-12-30 23:41:11 -0500

    Added dub support

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Library Compilations
-dmd core/*.d -lib -ofcore -w
-dmd core/socialmedia/*.d -lib -ofsocialmedia -w
-dmd jarvis.d core.a socialmedia.a
-#Cleanup
-rm *.o *.a
-
-echo "FirstTime = true" > State.txt
---
+++ source/app.d
@@ -0,0 +1,8 @@
+import std.stdio;
+import jarvis.core.user;
+
+void main(){
+	//Begins introduction
+	writeln("Welcome (placeholder).");
+	
+}
